c243e7e024e47f4f9a38e8d2317886e7527a06e9
Dev HK <devhk@override.local>
2016-07-19 15:49:09 +0800

    add function in util

--- src/utils.js
+++ src/utils.js
@@ -65,11 +65,21 @@ GetMethodsFromModel = (model) => {
   fromShared.forEach((shared) => { models.push(shared); })
 
   return models;  
+},
+randomId = () => {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 20; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
 };
 
 export { 
   BinderHelper, 
   Hook, 
   EnableDisableRemoteMethods, 
-  GetMethodsFromModel 
+  GetMethodsFromModel,
+  randomId 
 }
